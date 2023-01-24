39433d8f4678f5238737a8111ca2fde293168296
Lucian Buzzo <lucian.buzzo@gmail.com>
2023-01-24 13:25:00 +0000

    fix: Don't rerun middleware when applying RLC policies
    This change fixes an issue where middlewares created by the client would
    be run multiple times per query.
    
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

---
+++ test/integration/middleware.spec.ts
@@ -0,0 +1,103 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+import { createNamespace } from "cls-hooked";
+import { v4 as uuid } from "uuid";
+import { setup } from "../../src";
+
+let adminClient: PrismaClient;
+
+beforeAll(async () => {
+	adminClient = new PrismaClient();
+});
+
+describe("middlewares", () => {
+	it("should not run twice", async () => {
+		const prisma = new PrismaClient();
+
+		const middlewareSpy = jest.fn(async (params, next) => {
+			return next(params);
+		});
+
+		prisma.$use(middlewareSpy);
+
+		const role = `USER_${uuid()}`;
+
+		await setup({
+			prisma,
+			getRoles(abilities) {
+				return {
+					[role]: [abilities.Post.read, abilities.Post.create],
+				};
+			},
+			getContext: () => {
+				return {
+					role,
+					context: {},
+				};
+			},
+		});
+
+		middlewareSpy.mockClear();
+
+		const post = await prisma.post.create({
+			data: {
+				title: `Test post from ${role}`,
+			},
+		});
+
+		expect(post.id).toBeDefined();
+		expect(middlewareSpy).toHaveBeenCalledTimes(1);
+	});
+
+	it("should not be able to bypass RBAC when using cls-hooked", async () => {
+		const prisma = new PrismaClient();
+
+		const middleware: Prisma.Middleware = async (params, next) => {
+			if (params.model === "Post") {
+				const post = await next(params);
+				return post;
+			} else {
+				return next(params);
+			}
+		};
+
+		const clsSession = createNamespace("test");
+
+		prisma.$use(middleware);
+
+		const roleName = `USER_${uuid()}`;
+
+		await setup({
+			prisma,
+			getRoles(abilities) {
+				return {
+					[roleName]: [abilities.Post.read],
+				};
+			},
+			getContext: () => {
+				const role = clsSession.get("role");
+				return {
+					role,
+					context: {},
+				};
+			},
+		});
+
+		await expect(
+			new Promise((res, reject) => {
+				clsSession.run(async () => {
+					try {
+						clsSession.set("role", roleName);
+						const result = await prisma.post.create({
+							data: {
+								title: `Test post from ${roleName}`,
+							},
+						});
+						res(result);
+					} catch (e) {
+						reject(e);
+					}
+				});
+			}),
+		).rejects.toThrow();
+	});
+});
