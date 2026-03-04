bd5d5f44a10e70943884c8fd3b670aecbbfb4176
Lucian Buzzo <lucian.buzzo@gmail.com>
2026-03-04 10:18:48 +0000

    Revert "handle relation create checks"
    This reverts commit eb4e1036914daaa73950c7eb0e431eaab5b6026c.

--- test/integration/sanitation.spec.ts
+++ test/integration/sanitation.spec.ts
@@ -231,7 +231,7 @@ describe("sanitation", () => {
 		).rejects.toThrow("You do not have permission to perform this action");
 	});
 
-	it("should reject create checks when relation filters cannot be resolved from data", async () => {
+	it("should reject unsupported relation filters in create checks", async () => {
 		const initial = new PrismaClient();
 
 		const role = `USER_${uuid()}`;
@@ -273,8 +273,6 @@ describe("sanitation", () => {
 					title: "test",
 				},
 			}),
-		).rejects.toThrow(
-			"You do not have permission to perform this action: Post.create(...)",
-		);
+		).rejects.toThrow("Relation filters are not supported in create checks");
 	});
 });
